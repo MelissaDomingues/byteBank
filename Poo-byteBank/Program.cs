42ea017c7aa08b1892bb0dd6567135c60213f427
Melissa Rios <dominguesriosmel@gmail.com>
2022-11-29 18:28:11 -0300

    Adicionar arquivos de projeto.

---
+++ Poo-byteBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using Poo_byteBank;
+
+contaCorrente contaDoAndre = new contaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.n_Agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Titular da conta: " + contaDoAndre.titular);
+Console.WriteLine("Número da agência: " + contaDoAndre.n_Agencia);
+Console.WriteLine("Conta: " + contaDoAndre.conta);
+Console.WriteLine("Saldo da conta: " + contaDoAndre.saldo);
---
+++ Poo-byteBank/contaCorrente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_byteBank
+{
+    public class contaCorrente
+    {
+        public int n_Agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+            
+            
+    }
+}
